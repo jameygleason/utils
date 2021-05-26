c92488007e3809b0dce8312e2009d118dfa9aa22
jameygleason <james.p.gleason@gmail.com>
2021-05-25 21:49:05 -0400

    rimraf tests

--- utils/rimraf.ts
+++ utils/rimraf.ts
@@ -1,20 +1,12 @@
 import fs from "fs"
 import path from "path"
-import kleur from "kleur"
-import { isEmpty } from "./isEmpty" // .ts
 
 export function rimraf(entryPath: string): void {
-  if (isEmpty(entryPath)) {
-    throw new Error(kleur.red("rimraf requires a path to a directory or file"))
-  }
-
   if (!fs.existsSync(entryPath)) {
     return
   }
 
-  const stats = fs.statSync(entryPath)
-
-  if (stats.isDirectory()) {
+  if (fs.statSync(entryPath).isDirectory()) {
     fs.readdirSync(entryPath).forEach(entry => {
       rimraf(path.join(entryPath, entry))
     })
