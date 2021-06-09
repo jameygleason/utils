aa273f76d6968823ada052b1f78173aa04291d6f
jameygleason <james.p.gleason@gmail.com>
2021-06-09 16:58:11 -0400

    starting title case

---
+++ utils/titleCase.test.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai"
+import { titleCase } from "./titleCase"
+
+const apStyle = [{ input: "", output: "" }]
+
+const chicagoManuelOfStyle = [{ input: "", output: "" }]
+
+describe("titleCase", () => {
+  it.skip("Title cases string in the AP style", () => {
+    for (const { input, output } of apStyle) {
+      const [data, err] = titleCase(input)
+      if (err !== null) {
+        expect(err).to.eql(true)
+      }
+      expect(data).to.eql(output)
+    }
+  })
+
+  it.skip("Title cases string in the Chicago Manuel of Style style", () => {
+    for (const { input, output } of chicagoManuelOfStyle) {
+      const [data, err] = titleCase(input)
+      if (err !== null) {
+        expect(err).to.eql(true)
+      }
+      expect(data).to.eql(output)
+    }
+  })
+})
---
+++ utils/titleCase.ts
@@ -0,0 +1,8 @@
+export function titleCase(str: string): string {
+  let tcStr = ""
+  for (let i = 0; i < str.length; i++) {
+    tcStr = tcStr.trim()
+  }
+
+  return tcStr
+}
