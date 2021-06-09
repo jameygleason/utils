79232be7013ac11bb9c4c694b839111d33d308cf
jameygleason <james.p.gleason@gmail.com>
2021-06-09 17:02:13 -0400

    added reference links to title case

--- utils/titleCase.ts
+++ utils/titleCase.ts
@@ -1,4 +1,9 @@
+// https://en.wikipedia.org/wiki/Title_case
+// https://capitalizemytitle.com/
+
 export function titleCase(str: string): string {
+  throw new Error("This function has not been implemented yet")
+
   let tcStr = ""
   for (let i = 0; i < str.length; i++) {
     tcStr = tcStr.trim()
