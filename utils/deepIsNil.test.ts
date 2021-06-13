00ed406faaf5675179fe82ff39c36a0cb5826da2
jameygleason <james.p.gleason@gmail.com>
2021-06-13 07:34:17 -0400

    added deep is nil

---
+++ utils/deepIsNil.test.ts
@@ -0,0 +1,8 @@
+import { assert } from "chai"
+// import { deepIsNil } from "./deepIsNil"
+
+describe("deepIsNil", () => {
+  it.skip("TODO", () => {
+    assert.strictEqual("TODO", true)
+  })
+})
---
+++ utils/deepIsNil.ts
@@ -0,0 +1,37 @@
+import { isNil } from "./isNil"
+
+type Types = undefined | null | string | number | Array<any> | Object
+
+const empty = []
+let i = 0
+
+export function deepIsNil(arg: Types): any {
+  try {
+    if (isNil(arg)) {
+      return isNil(arg)
+    }
+    if (arg.constructor === Array) {
+      for (const a of arg) {
+        const item = deepIsNil(a)
+        if (item) {
+          empty[i].push(a)
+        }
+      }
+    }
+
+    if (arg.constructor === Object) {
+      for (const [k, v] of Object.entries(arg)) {
+        const item = deepIsNil(v)
+        if (item) {
+          empty[i].push(Object.fromEntries([k, v]))
+        }
+      }
+    }
+
+    i++
+
+    return [empty, null]
+  } catch (err) {
+    console.error(err)
+  }
+}
