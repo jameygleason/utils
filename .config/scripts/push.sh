8a9193f689d6887defb0f218a90be2c1667e04f3
jameygleason <james.p.gleason@gmail.com>
2021-10-04 16:35:55 -0400

    fixed package.json deps oopsy

--- .config/scripts/push.sh
+++ .config/scripts/push.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
+# Github
 git remote set-url origin git@github.com:jameygleason/utils.git && \
 git push origin && \
+git push --tags && \
+
+# Gitlab
 git remote set-url origin git@gitlab.com:jameygleason/utils.git && \
-git push origin
+git push origin && \
+git push --tags
