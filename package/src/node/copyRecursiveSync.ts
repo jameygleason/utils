8b2b6c8930d79fe8ff4d3eff7c3779c8085f546a
jameygleason <james.p.gleason@gmail.com>
2022-04-07 09:49:28 -0400

    released -> 0.8.0 added copyRecursiveSync

---
+++ package/src/node/copyRecursiveSync.ts
@@ -0,0 +1,21 @@
+import fs from "fs"
+import path from "path"
+import { mkdir } from "./mkdir"
+
+export function copyRecursiveSync(src: string, dest: string): void {
+	let isDirectory = false
+	if (fs.existsSync(src)) {
+		let stats = fs.statSync(src)
+		isDirectory = stats.isDirectory()
+	}
+
+	if (isDirectory) {
+		mkdir(dest)
+		fs.readdirSync(src).forEach(childItemName => {
+			copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName))
+		})
+		return
+	}
+
+	fs.copyFileSync(src, dest)
+}
