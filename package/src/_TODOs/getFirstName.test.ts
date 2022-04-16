b2963e4d2da134b8ecd84dda73dedbd55aa9630f
jameygleason <james.p.gleason@gmail.com>
2022-04-16 09:19:19 -0400

    moved todo packages to todos dir

---
+++ package/src/_TODOs/getFirstName.test.ts
@@ -0,0 +1,73 @@
+// import { describe, it, assert } from "vitest"
+// import { getFirstName } from "../dist/getFirstName.js"
+
+// const testCases = [
+// 	{
+// 		input: "😘 Emoji Test",
+// 		output: "Emoji",
+// 	},
+// 	{
+// 		input: "😘 Emoji ❤️ Test",
+// 		output: "Emoji",
+// 	},
+// 	{
+// 		input: "💯👍🔥 Emoji ❤️ Test",
+// 		output: "Emoji",
+// 	},
+// 	{
+// 		input: "Jimi Hendrix",
+// 		output: "Jimi",
+// 	},
+// 	{
+// 		input: "  Lindsey Buckingham", // trim front
+// 		output: "Lindsey",
+// 	},
+// 	{
+// 		input: "James Hetfield   ", // trim end
+// 		output: "James",
+// 	},
+// 	{
+// 		input: "andy Summers", // lowercase first string
+// 		output: "Andy",
+// 	},
+// 	{
+// 		input: "willie nelson", // lowercase all strings
+// 		output: "Willie",
+// 	},
+// 	{
+// 		input: "Jack white", // lowercase last string
+// 		output: "Jack",
+// 	},
+// 	{
+// 		input: "   stevie    ray     vaughn    ", // egregious usage of spaces and all lowercase
+// 		output: "Stevie",
+// 	},
+// 	{
+// 		input: "bç𝛀a🍌nanas string",
+// 		output: "Bananas",
+// 	},
+// 	{
+// 		input: "break case",
+// 		output: "break",
+// 	},
+// ]
+
+// describe("getFirstName", () => {
+// 	it("Capitalizes the first letter of the first word in a string", () => {
+// 		for (const { input, output } of testCases) {
+// 			const [data, error] = getFirstName(input)
+
+// 			if (error !== null) {
+// 				assert.strictEqual("This case should not happen", "true")
+// 				return
+// 			}
+
+// 			if (input === "break case") {
+// 				assert.strictEqual("break", output)
+// 				return
+// 			}
+
+// 			assert.strictEqual(data, output)
+// 		}
+// 	})
+// })
